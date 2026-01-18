db8018b8cfd898c3ec7a827434c7cd2999c4ab1f
daria-chr <cherkinadar@gmail.com>
2026-01-18 06:23:10 +0300

    Employee class with private fields

---
+++ src/Employee.kt
@@ -0,0 +1,10 @@
+class Employee {
+    var fullName: String=""
+        private set
+    var position: String=""
+        private set
+    var salary: Int=0
+        private set
+    var yearsOfExperience: Int=0
+        private set
+}
