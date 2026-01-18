e509b8caf9d7a0545822add94f67b543aa01cb86
daria-chr <cherkinadar@gmail.com>
2026-01-18 09:02:39 +0300

    Implement assignTask method with validation

--- src/Employee.kt
+++ src/Employee.kt
@@ -4,6 +4,7 @@ class Employee (
     salary: Int,
     yearsOfExperience: Int) :ReportGenerator
 {
+    var currentTask: Task?=null
     var fullName: String=fullName
         get() = field
     var position: String=position
@@ -27,10 +28,21 @@ class Employee (
     override fun generateReport(): String {
         return "===Отчет о сотруднике===\n ФИО: $fullName\n Должность: $position\n Зарплата: $salary\n Опыт работы: $yearsOfExperience"
     }
+    fun assignTask(newTask: Task) {
+        val cur=currentTask
+        if (cur!= null && cur.isCompleted){
+            println("Сотрудник уже занят задачей ${cur.title}")
+        }else{
+            currentTask=newTask
+            println("Задача ${newTask.title} назначена")
+        }
+    }
 }
 
 fun main() {
     val employee=Employee("Александров Александр Александрович","Менеджер",50000, 6)
+    val task1=Task("Работа", "сделать все задания",Priority.HIGH)
+    val task2=Task("Отдых", "спать",Priority.LOW)
     println("Зарплата ${employee.salary}")
     employee.salary=-3
     employee.salary=55000
@@ -39,4 +51,14 @@ fun main() {
     println("Опыт ${employee.yearsOfExperience}")
     employee.yearsOfExperience=90
     println("Опыт ${employee.yearsOfExperience}")
+
+    println("Фио ${employee.fullName}")
+    println("Должность ${employee.position}")
+    println("Зп ${employee.salary}")
+    println("Опыт работы ${employee.yearsOfExperience}")
+    val developmentDepartment=DevelopmentDepartment()
+    println(developmentDepartment.generateReport())
+
+    employee.assignTask(task1)
+    employee.assignTask(task2)
 }
